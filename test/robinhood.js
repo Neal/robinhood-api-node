5899f6682465b60f12700b27bfcaab692e176dbe
Neal Patel <neal@ineal.me>
2016-05-04 17:07:45 -0700

    Moar tests

--- test/robinhood.js
+++ test/robinhood.js
@@ -26,6 +26,8 @@ const fakeDataWithAuthToken = {
 
 describe('Robinhood', () => {
 
+  let rh;
+
   beforeEach(done => {
     nock.cleanAll();
 
@@ -45,56 +47,220 @@ describe('Robinhood', () => {
         results: [{account_number: 'someAccountNumber'}] // eslint-disable-line camelcase
       });
 
-    done();
+    rh = new Robinhood(fakeDataWithAuthToken, err => {
+      should.not.exist(err);
+      should.exist(rh._accountNumber);
+      done();
+    });
   });
 
   afterEach(done => {
     done();
   });
 
-  it('should set username, password, authToken, accountNumber and apiRoot', () => {
-    let rh = new Robinhood(fakeData);
-    rh._username.should.equal(fakeData.username);
-    rh._password.should.equal(fakeData.password);
-    rh._authToken.should.equal(fakeData.authToken);
-    rh._accountNumber.should.equal(fakeData.accountNumber);
-    rh._apiRoot.should.equal(fakeData.apiRoot);
-  });
-
-  it('should handle callback', () => {
-    let rh = new Robinhood(fakeData, (err, data) => {
+  it('should set username, password, authToken, accountNumber and apiRoot', done => {
+    let rh = new Robinhood(fakeData, err => {
       should.not.exist(err);
-      should.exist(data);
+      rh._username.should.equal(fakeData.username);
+      rh._password.should.equal(fakeData.password);
+      rh._authToken.should.equal(fakeData.authToken);
+      rh._accountNumber.should.equal(fakeData.accountNumber);
+      rh._apiRoot.should.equal(fakeData.apiRoot);
+      done();
     });
   });
 
-  it('should handle promise', () => {
-    let rh = new Robinhood(fakeData, (err, data) => {
+  it('should handle callback', done => {
+    rh.accounts.getAll((err, data) => {
       should.not.exist(err);
       should.exist(data);
-      rh.accounts.getAll()
-        .then(data => {
-          should.exist(data);
-        })
-        .catch(err => {
-          should.not.exist(err);
-        });
+      done();
     });
+  });
+
+  it('should handle promise', done => {
+    rh.accounts.getAll()
+      .then(data => {
+        should.exist(data);
+      })
+      .catch(err => {
+        should.not.exist(err);
+      })
+      .then(done);
   });
 
   it('should get accountNumber', done => {
-    let rh = new Robinhood(fakeDataWithAuthToken, err => {
-      should.not.exist(err);
-      should.exist(rh._authToken);
-      rh._getAccountNumber()
-        .then(data => {
-          should.exist(rh._accountNumber);
-        })
-        .catch(err => {
-          should.not.exist(err);
-        })
-        .then(done);
+    rh._getAccountNumber()
+      .then(data => {
+        should.exist(rh._accountNumber);
+      })
+      .catch(err => {
+        should.not.exist(err);
+      })
+      .then(done);
+  });
+
+  describe('buildReqOpts', () => {
+
+    it('should throw error with no method', done => {
+      let opts = {
+        endpoint: '/test/'
+      };
+
+      rh._buildReqOpts(opts, err => {
+        should.exist(err);
+        done();
+      });
     });
+
+    it('should throw error with no endpoint', done => {
+      let opts = {
+        method: 'GET'
+      };
+
+      rh._buildReqOpts(opts, err => {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should accept custom method', done => {
+      let opts = {
+        method: 'POST',
+        endpoint: '/test/'
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.exist(reqOpts.method);
+        reqOpts.method.should.equal(opts.method);
+        done();
+      });
+    });
+
+    it('should accept custom endpoint', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/'
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.exist(reqOpts.url);
+        reqOpts.url.should.equal(rh._apiRoot + opts.endpoint);
+        done();
+      });
+    });
+
+    it('should accept custom headers', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/',
+        headers: {
+          'accept': 'application/json'
+        }
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.exist(reqOpts.headers.accept);
+        reqOpts.headers.accept.should.equal(opts.headers.accept);
+        done();
+      });
+    });
+
+    it('should accept custom query params', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/',
+        qs: {
+          'symbol': 'AAPL'
+        }
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.exist(reqOpts.qs.symbol);
+        reqOpts.qs.symbol.should.equal(opts.qs.symbol);
+        done();
+      });
+    });
+
+    it('should handle noAuthToken flag', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/',
+        noAuthToken: true
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.not.exist(reqOpts.headers.Authorization);
+        done();
+      });
+    });
+
+    it('should set auth header', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/'
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.exist(reqOpts.headers.Authorization);
+        reqOpts.headers.Authorization.should.equal('Token ' + rh._authToken);
+        done();
+      });
+    });
+
+    it('should throw if no auth token', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/'
+      };
+
+      delete rh._authToken;
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should handle POST form', done => {
+      let opts = {
+        method: 'POST',
+        endpoint: '/test/',
+        form: {
+          'symbol': 'AAPL'
+        }
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.exist(reqOpts.form.symbol);
+        reqOpts.form.symbol.should.equal(opts.form.symbol);
+        done();
+      });
+    });
+
+    it('should discard form if not POST', done => {
+      let opts = {
+        method: 'GET',
+        endpoint: '/test/',
+        form: {
+          'symbol': 'AAPL'
+        }
+      };
+
+      rh._buildReqOpts(opts, (err, reqOpts) => {
+        should.not.exist(err);
+        should.not.exist(reqOpts.form);
+        done();
+      });
+    });
+
   });
 
 });
